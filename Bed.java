575dabf21aee871b3222a2fe181c041739b88a2b
Sarah Miller <smiller807@smith.edu>
2025-11-22 18:10:46 -0500

    added classes for the bedroom

--- Bed.java
+++ Bed.java
@@ -10,10 +10,21 @@ public class Bed {
     public void liftCovers(){
         if(!lifted){
             lifted = true;
-            clueFound = true;
-            System.out.println("You have lifted the covers."); // add clue
+            System.out.println("You have lifted the covers."); // add indication of a clue
         } else {
             System.out.println("The covers are already lifted.");
         }
     }
+
+    public void pickUp(){
+        if(lifted && clueFound){
+            clueFound = true;
+            System.out.println("You found a clue under the covers!");
+        } else if(!lifted){
+            System.out.println("You need to lift the covers first.");
+        } else {
+            System.out.println("You have already found the clue under the covers.");
+        }
+    }
+
 }
