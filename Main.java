0e0fe6e0ba23f408e84ef58cd6b78a65679807c6
Isabella Purnell-Amaez <ipurnellamaez@smith.edu>
2025-11-22 19:28:46 -0500

    11/22

--- Main.java
+++ Main.java
@@ -3,8 +3,6 @@ public class Main {
    private Cup blueCup = new Cup("Inside the blue cup is a piece of paper with the number '4' on it");
    private Cup greenCup = new Cup("Inside the red cup is a piece of paper with the number '4' on it");
 
-   
-
 
     //private int pieceNumber = ; //number of puzzle pieces found & on the player's person.
 
