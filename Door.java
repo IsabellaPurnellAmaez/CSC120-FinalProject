f241248f3586355f68909d222fd2ecd2289f639a
Isabella Purnell-Amaez <ipurnellamaez@smith.edu>
2025-12-11 09:30:30 -0500

    12/11

--- Door.java
+++ Door.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 public class Door {
     String name;
     boolean isLocked;
--- Puzzle.java
+++ Puzzle.java
@@ -1,31 +1,39 @@
 import java.util.ArrayList;
 
 public class Puzzle {
-    ArrayList<Integer> pieces; //array list of puzzle pieces*** 
+    ArrayList<String> pieces; //array list of puzzle pieces*** 
+    Boolean flipped;
 
     public Puzzle(){
         this.pieces = new ArrayList<>();
         for(int i = 1; i <= 3; i++){
-            pieces.add(i);
+            pieces.add("puzzle piece " + i);
         }
-
+        this.flipped = false;
     }
-    public void placePiece(int pieceNumber){ // have piece number be some sort of variable in main OR check if piece in pocket instead.***
+
+    public Boolean getFlipped(){
+        return this.flipped;
+    }
+    
+    public void placePiece(String pieceNumber){ // have piece number be some sort of variable in main OR check if piece in pocket instead.***
         if(pieces.contains(pieceNumber)){
-            pieces.remove(Integer.valueOf(pieceNumber));
+            pieces.remove(pieceNumber);
             //have this message say "you have placed *some number* of pieces." then say if the puzzle is done or not. 
             System.out.println("You have placed piece " + pieceNumber + " into the puzzle.");
             }
         else {
             System.out.println("You don't have any pieces to finish the puzzle, go find them!");
         }
+
     }
 
     public void flipPuzzle(){
-        if(this.pieces.size() == 3 ){
-            System.out.println("You flip the completed puzzle to reveal a hidden message."); // add message
+        if(this.pieces.size() == 0 ){
+            System.out.println("You flip the completed puzzle to reveal a hidden message: Look behind the mirror"); // add message
+            this.flipped = true;
         } else {
-            System.out.println("The puzzle is not yet completed. You cannot flip it.");
+            System.out.println("The flipped puzzle has a message on it but there are still too many pieces to understand what it says. Go look for more pieces and then try to flip it again.");
         }
     }
 
